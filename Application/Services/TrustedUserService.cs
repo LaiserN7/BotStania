589ed4a317a3c6f556dae5260224a76f89b2e918
laisern7 <laisern7.work@gmail.com>
2022-04-13 20:10:09 +0300

    Some edit

--- Application/Services/TrustedUserService.cs
+++ Application/Services/TrustedUserService.cs
@@ -6,20 +6,20 @@ namespace Application.Services;
 
 public class TrustedUserService : ITrustedUsersService
 {
-    private ConcurrentDictionary<long, TrustedUser> TrustedUsers = new();
+    private readonly ConcurrentDictionary<long, TrustedUser> _trustedUsers = new();
 
-    public bool Add(TrustedUser user) => TrustedUsers.TryAdd(user.UserId, user);
+    public bool Add(TrustedUser user) => _trustedUsers.TryAdd(user.UserId, user);
 
-    public bool Remove(long userId) => TrustedUsers.TryRemove(userId, out _);
+    public bool Remove(long userId) => _trustedUsers.TryRemove(userId, out _);
 
     public bool Remove(string name)
     {
-        var element = TrustedUsers.Values.FirstOrDefault(x => x.Name == name);
-        return element is not null && TrustedUsers.TryRemove(element.UserId, out _);
+        var element = _trustedUsers.Values.FirstOrDefault(x => x.Name == name);
+        return element is not null && _trustedUsers.TryRemove(element.UserId, out _);
     }
 
-    public bool IsExist(long userId) => TrustedUsers.ContainsKey(userId);
+    public bool IsExist(long userId) => _trustedUsers.ContainsKey(userId);
 
-    public bool IsExist(string name) => TrustedUsers.Values.Any(x => x.Name == name);
-    public bool IsEmpty() => TrustedUsers.IsEmpty;
+    public bool IsExist(string name) => _trustedUsers.Values.Any(x => x.Name == name);
+    public bool IsEmpty() => _trustedUsers.IsEmpty;
 }
